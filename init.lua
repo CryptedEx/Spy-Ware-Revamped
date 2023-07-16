20596508baf9f778f25d9510254508063115d6d1
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2023-07-16 19:30:40 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,7 @@
+local plr = game.Players.LocalPlayer.Name
+local plrid = game.Players.LocalPlayer.UserId
+if game.PlaceId == 10449761463 then
+  print("The Strongest Battlegrounds")
+else
+  plr:kick("Wrong game dummy."
+  end;
