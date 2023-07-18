6283222c23b93453e712b4f60c8055c0e919a465
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2023-07-18 20:17:44 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@ if game.PlaceId == 10449761463 then
   print("The Strongest Battlegrounds")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptedEx/Spy-Ware-Revamped/main/Games/The%20Strongest%20Battlegrounds.lua"))()
 else
+if game.PlaceId == 621129760 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptedEx/Spy-Ware-Revamped/main/Games/The%20Strongest%20Battlegrounds.lua"))()
+else
   plr:kick("Wrong game dummy.")
   end;
